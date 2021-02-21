49a86f3e5bb31cf23b1cfdea7c4154d03fb35759
Parfait Munezero <parfait.munezero@ericsson.com>
2021-02-21 13:01:19 +0100

    Added vignette and refactored the code.

--- R/group_survival_data.R
+++ R/group_survival_data.R
@@ -11,76 +11,88 @@
 #' @return A data frame containing the grouped survival data.
 #' @export
 
-group_surv<-function(surv_data,time_intervals,col_indexes)
+grouping<-function(survData,intervals,T_ind,C_ind,X_vec)
 {
-  column_names = c("Exposure","Events","Group",colnames(surv_data)[col_indexes$variables])
-  grouped_data<-c()
-  for(i in seq_len(length(time_intervals)-1)){
-    new_data <- interval_data(lower_limit=time_intervals[i],
-                              upper_limit=time_intervals[i+1],
-                              surv_data=surv_data,
-                              time_index=col_indexes$time,
-                              event_index=col_indexes$event
-                              )
-    variable_unique_labels<-as.data.frame(unique(new_data[,col_indexes$variables]))
+  grouped<-c()
+  for(i in 2:length(intervals))
 
-    for(j in seq_len(nrow(variable_unique_labels))){
-      variable_data <- as.matrix(new_data[,col_indexes$variables]) == matrix(rep(variable_unique_labels[j,],
-                                                                          each=nrow(new_data)),
-                                                                      ncol=length(variable_unique_labels[j,]
-                                                                                  )
-                                                                      )
+  {
+    Data.new<-IntervalData(survData,intervals[i-1],intervals[i],T_ind,C_ind)
+    L<-dim(Data.new)[1]
+    X.mat<-as.data.frame(unique(Data.new[,X_vec]))
+    for(j in 1:dim(X.mat)[1])
+    {
+      # difference between a given variable combination with the whole dataset
+      Diff<-as.matrix(Data.new[,X_vec])==
+        matrix(rep(X.mat[j,],each=L),ncol=length(X.mat[j,]))
 
-      variable_data <- ifelse(variable_data==TRUE,1,0)
-      exposure <- sum(new_data[which(apply(variable_data,1,prod)==1),col_indexes$time])
-      number_events <- sum(new_data[which(apply(variable_data,1,prod)==1),col_indexes$event])
-      grouped_data <- rbind(grouped_data,cbind(exposure=exposure,events=number_events,
-                                             time_group=i-1,variable_unique_labels[j,]))
+      Diff<-ifelse(Diff==TRUE,1,0)
+      Exposure<-sum(Data.new[which(apply(Diff,1,prod)==1),T_ind])
+      Events<-sum(Data.new[which(apply(Diff,1,prod)==1),C_ind])
+      grouped<-rbind(grouped,cbind(Exposure=Exposure,Events=Events,Group=i-1,X.mat[j,]))
     }
 
   }
-  colnames(grouped_data) <- column_names
-  return(grouped_data)
+
+  colnames(grouped)<-c("Exposure","Events","Group",colnames(survData)[X_vec])
+  rownames(grouped) <- c()
+
+  return(as.data.frame(grouped))
 }
+
 
 #' Extract interval-specific data
 #'
 #' Extract data for observations  which experience or are censored within a specified time interval.
 #' @param lower_limit The lower limit of the time interval
 #' @param upper_limit The upper limit of the time interval
+#' @param surv_data The raw data in the foarm of a data frame.
+#' @param col_indexes A list of column indexes that specify the columns index  of the survival time,
+#'  censoring indicator, and covariates used.
 #' @return The interval-specific data in the foarm of a data frame.
+#' @export
 
-interval_data<-function(lower_limit,upper_limit,...){
+IntervalData<-function(survData,LowLim,UPLim,T_ind,C_ind)
+{
   # filter individuals who experienced the event or were censored before the interval [LowLim,UPLim)
-  interval_data<-surv_data[which(surv_data[,time_index] >= lower_limit),]
+  IntData<-survData[which(survData[,T_ind]>=LowLim),]
   # compute the new survival time
-  exposure<-apply(cbind(interval_data[,time_index]-lower_limit,
-                        rep((upper_limit-lower_limit),nrow(interval_data))), 1, min)
-
-  # as a reminder: an obseration is censored if it survives beyond the interval or was censored within the interval.
-  # if an observation is censored the index is assigned 0, otherwise it is 1.
-  event<-interval_data[,event_index]*ifelse(interval_data[,time_index] < upper_limit,1,0)
-  return(data.frame(exposure=exposure,event=event,interval_data[,-c(time_index,event_index)]))
-  }
+  SurvTime<-apply(cbind(IntData[,T_ind]-LowLim,rep((UPLim-LowLim),
+                                                   nrow(IntData))),1,min)
+  # compute the interval censoring index
+  # as a reminder: an obs is censored if it survive beyond the interval or was censored within the interval.
+  # if an obs is censored the index is given calue equal to 0, otherwise it is 1.
+  index<-IntData[,C_ind]*ifelse(IntData[,T_ind]<UPLim,1,0)
+  return(data.frame(Time=SurvTime,Status=index,IntData[,-c(T_ind,C_ind)]))
+}
 
 
 #' Compute a summary of the grouped data
-#' @param grouped_data A data frame containing the grouped data to be summarized.
-#' @param time_intervals A vector of time points partitioning the survival time. They define the survival time groups.
-#' @param col_indexes A vector of column indexes for which to display the summary.
+#' @param grouped.data A data frame containing the grouped data to be summarized.
+#' @param intervals A vector of time points partitioning the survival time. They define the survival time groups.
+#' @param col.indexes A vector of column indexes for which to display the summary.
 #' @return The data summary.
-summarize<-function(grouped_data,time_intervals,col_indexes=c(1,2)){
-  grp_names<-c()
-  total_summary<-c()
-  for (i in seq_len(length(time_intervals)-1)){
+#' @export
 
-    grp_names<-cbind(grp_names,
-                     paste0("[",paste0(time_intervals[i]),
-                            paste0(",", paste0(time_intervals[i+1],"["))))
-    total_summary <- rbind(total_summary,apply(grouped_data[which(grouped_data$Group==i),col_indexes],2,sum))
+grouped_summary<-function(grouped.data,intervals,col.indexes=c(1,2))
+{
+  grp.names<-c()
+  total.sum<-c()
+  for (i in 1:(length(intervals)-1))
+  {
+
+    grp.names<-cbind(grp.names,
+                     paste0("[",paste0(intervals[i]),
+                            paste0(",", paste0(intervals[i+1],")"))))
+    total.sum<-rbind(total.sum,apply(grouped.data[which(grouped.data$Group==i),
+                                                  col.indexes],2,sum))
 
   }
-  rownames(total_summary) <- grp_names
-  return(total_summary)
+  grand_totals <- c(Total=apply(total.sum,2,sum))
+  total.sum <- rbind(total.sum , grand_totals)
+  rownames(total.sum)<-c(grp.names, 'Total')
+  #print(c(Total=apply(total.sum,2,sum)))
+  return(total.sum)
+
 }
 
